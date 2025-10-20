665f0afb639b445b01c186703cc0a16b52f381a8
aiden-bitcoin <aiden@fiammachain.io>
2025-10-20 11:08:44 +0800

    feat: upgrade btc verifier

--- packages/contracts/src/BtcTxVerifier.sol
+++ packages/contracts/src/BtcTxVerifier.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.18;
 
 import "./interfaces/IBtcMirror.sol";
 import "./interfaces/IBtcTxVerifier.sol";
 import "./BtcProofUtils.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 //
 //                                        #
@@ -28,10 +29,21 @@ import "./BtcProofUtils.sol";
 // BtcVerifier implements a merkle proof that a Bitcoin payment succeeded. It
 // uses BtcMirror as a source of truth for which Bitcoin block hashes are in the
 // canonical chain.
-contract BtcTxVerifier is IBtcTxVerifier {
-    IBtcMirror public immutable mirror;
+contract BtcTxVerifier is OwnableUpgradeable, IBtcTxVerifier {
+    IBtcMirror public mirror;
 
-    constructor(address _mirror) {
+    /// @dev Custom errors
+    error InvalidOwnerAddress();
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address _owner, address _mirror) external initializer {
+        if (_owner == address(0)) revert InvalidOwnerAddress();
+
+        __Ownable_init(_owner);
         mirror = IBtcMirror(_mirror);
     }
 
@@ -46,8 +58,6 @@ contract BtcTxVerifier is IBtcTxVerifier {
         uint256 opReturnOutIx,
         bytes32 opReturnData
     ) external view returns (bool) {
-        checkHeight(blockNum, minConfirmations);
-
         bytes32 blockHash = mirror.getBlockHash(blockNum);
         return BtcProofUtils.validatePayment(
             blockHash, inclusionProof, txOutIx, destScriptHash, amountSats, checkOpReturn, opReturnOutIx, opReturnData
@@ -70,4 +80,7 @@ contract BtcTxVerifier is IBtcTxVerifier {
         require(mirrorHeight >= blockNum, "Bitcoin Mirror doesn't have that block yet");
         require(mirrorHeight + 1 >= minConfirmations + blockNum, "Not enough Bitcoin block confirmations");
     }
+
+    // reserved storage space to allow for layout changes in the future
+    uint256[50] private __gap;
 }
